756ebbdb4c337951b458cbbe1bdd24be119bb559
Rob3rttt <maleckas.robertas@gmail.com>
2015-05-07 22:11:31 +0300

    - reworked sidebar - cleaned up MainActivity - fixed string value in AddAssignment

--- src/com/coffeetocode/assignmentreminder/MainActivity.java
+++ src/com/coffeetocode/assignmentreminder/MainActivity.java
@@ -1,8 +1,5 @@
 package com.coffeetocode.assignmentreminder;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
@@ -11,54 +8,33 @@ import android.support.v4.widget.DrawerLayout;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
+import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.Toast;
 
-import com.coffeetocode.assignmentreminder.Assignment;
+import java.util.ArrayList;
+import java.util.List;
 
 @SuppressWarnings("deprecation")
 public class MainActivity extends Activity {
 
-    private class DrawerItemClickListener implements
-            ListView.OnItemClickListener {
-
-        @Override
-        public void onItemClick(AdapterView<?> parent, View view, int position,
-                                long id) {
-
-            // Highlight the selected item, update the title, and close the
-            // drawer
-            // update selected item and title, then close the drawer
-            mDrawerList.setItemChecked(position, true);
-
-            String text = "You clicked item #" + (position + 1);
-            Toast.makeText(MainActivity.this, text, Toast.LENGTH_SHORT).show();
-            cardArrayAdapter.remove(position);
-            mDrawerLayout.closeDrawer(mDrawerList);
-
-        }
-    }
-
-    private String[] mDrawerItems;
-    private DrawerLayout mDrawerLayout;
-    private ListView mDrawerList;
+    public final static int NEW_ASSIGNMENT_REQUEST = 1;
+    public List<Assignment> assignments = new ArrayList<Assignment>();
+    DBHandler dbHandler = new DBHandler(this);
+    private DrawerLayout drawerLayout;
+    private LinearLayout drawer;
     private ActionBarDrawerToggle mDrawerToggle;
     private CharSequence mTitle, mDrawerTitle;
     private ListView cardFeed;
-    public List<Assignment> assignments = new ArrayList<Assignment>();
     private CardArrayAdapter cardArrayAdapter;
-    DBHandler dbHandler = new DBHandler(this);
-    public final static int NEW_ASSIGNMENT_REQUEST = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mDrawerItems = getResources().getStringArray(R.array.drawer_item_names);
-        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
-        mDrawerList = (ListView) findViewById(R.id.left_drawer);
+
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer = (LinearLayout) findViewById(R.id.left_drawer);
         cardFeed = (ListView) findViewById(R.id.cards_feed);
         cardArrayAdapter = new CardArrayAdapter(this, R.layout.assignment_card);
 
@@ -66,33 +42,25 @@ public class MainActivity extends Activity {
 
         cardFeed.setAdapter(cardArrayAdapter);
 
-        // Set the adapter for the list view
-        mDrawerList.setAdapter(new ArrayAdapter<String>(this,
-                R.layout.drawer_list_item, mDrawerItems));
-        // Set the list's click listener
-        mDrawerList.setOnItemClickListener(new DrawerItemClickListener());
         mTitle = getTitle();
         mDrawerTitle = getTitle();
-        mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout,
+        mDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout,
                 R.drawable.ic_drawer, R.string.drawer_open,
                 R.string.drawer_close) {
             /** Called when a drawer has settled in a completely closed state. */
             public void onDrawerClosed(View view) {
                 super.onDrawerClosed(view);
                 getActionBar().setTitle(mTitle);
-                invalidateOptionsMenu(); // creates call to
-                // onPrepareOptionsMenu()
+                invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
             }
 
             /** Called when a drawer has settled in a completely open state. */
             public void onDrawerOpened(View drawerView) {
                 super.onDrawerOpened(drawerView);
                 getActionBar().setTitle(mDrawerTitle);
-                invalidateOptionsMenu(); // creates call to
-                // onPrepareOptionsMenu()
+                invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
             }
         };
-        mDrawerLayout.setDrawerListener(mDrawerToggle);
     }
 
     @Override
@@ -146,8 +114,14 @@ public class MainActivity extends Activity {
         // If the navigation drawer is open, hide action items related to the
         // content
         // view
-        boolean drawerOpen = mDrawerLayout.isDrawerOpen(mDrawerList);
+        boolean drawerOpen = drawerLayout.isDrawerOpen(drawer);
         menu.findItem(R.id.action_new).setVisible(!drawerOpen);
         return super.onPrepareOptionsMenu(menu);
+    }
+
+    // TODO: remove this funcion together with the sidebar's test button; add appropriate onClick funcion implementations for sidebar
+    public void doToast(View view) {
+        Toast.makeText(this, "Hello world", Toast.LENGTH_SHORT).show();
+        drawerLayout.closeDrawer(drawer);
     }
 }
