c87310e5ec445db29394a81cd9f32e4e55eb3227
JustasBelevicius <jube1997@gmail.com>
2015-05-07 22:30:46 +0300

    Added a useless comment for testing purposes.

--- build/generated/source/r/debug/com/coffeetocode/assignmentreminder/R.java
+++ build/generated/source/r/debug/com/coffeetocode/assignmentreminder/R.java
@@ -25,14 +25,14 @@ public final class R {
         public static final int ic_launcher=0x7f020006;
     }
     public static final class id {
-        public static final int action_accept=0x7f080010;
-        public static final int action_new=0x7f08000e;
-        public static final int action_settings=0x7f08000f;
+        public static final int action_accept=0x7f080011;
+        public static final int action_new=0x7f08000f;
+        public static final int action_settings=0x7f080010;
         public static final int cards_feed=0x7f080002;
         public static final int content_frame=0x7f080001;
         public static final int dateDisplay=0x7f080006;
         public static final int deleteReminder=0x7f080007;
-        public static final int delete_button=0x7f08000d;
+        public static final int delete_button=0x7f08000e;
         public static final int description=0x7f08000c;
         public static final int drawer_layout=0x7f080000;
         public static final int editText=0x7f080004;
@@ -40,6 +40,7 @@ public final class R {
         public static final int editText5=0x7f080005;
         public static final int left_drawer=0x7f080003;
         public static final int rel_layout=0x7f08000a;
+        public static final int subject=0x7f08000d;
         public static final int timeDisplay=0x7f080008;
         public static final int title=0x7f08000b;
     }
