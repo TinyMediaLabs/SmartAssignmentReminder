98f9d07c05a0e1753a04d06d446b5c699f4f9a7d
Rob3rttt <maleckas.robertas@gmail.com>
2015-05-08 15:32:17 +0300

    added color responsive button style to sidebar, cleaned up XMLs

--- build/generated/source/r/debug/com/coffeetocode/assignmentreminder/R.java
+++ build/generated/source/r/debug/com/coffeetocode/assignmentreminder/R.java
@@ -8,12 +8,16 @@
 package com.coffeetocode.assignmentreminder;
 
 public final class R {
+    public static final class array {
+        public static final int syncFrequency=0x7f050000;
+        public static final int syncFrequencyValues=0x7f050001;
+    }
     public static final class attr {
     }
     public static final class dimen {
-        public static final int activity_horizontal_margin=0x7f040000;
-        public static final int activity_padding=0x7f040001;
-        public static final int activity_vertical_margin=0x7f040002;
+        public static final int activity_horizontal_margin=0x7f060000;
+        public static final int activity_padding=0x7f060001;
+        public static final int activity_vertical_margin=0x7f060002;
     }
     public static final class drawable {
         public static final int card=0x7f020000;
@@ -24,28 +28,29 @@ public final class R {
         public static final int ic_action_settings=0x7f020005;
         public static final int ic_drawer=0x7f020006;
         public static final int ic_launcher=0x7f020007;
+        public static final int sidebar_button_background=0x7f020008;
     }
     public static final class id {
-        public static final int action_accept=0x7f080013;
-        public static final int action_new=0x7f080011;
-        public static final int action_settings=0x7f080012;
-        public static final int cards_feed=0x7f080008;
-        public static final int content_frame=0x7f080007;
-        public static final int dateDisplay=0x7f080002;
-        public static final int deleteReminder=0x7f080003;
-        public static final int delete_button=0x7f080010;
-        public static final int description=0x7f08000e;
-        public static final int drawer_layout=0x7f080006;
-        public static final int editText=0x7f080000;
-        public static final int editText2=0x7f080005;
-        public static final int editText5=0x7f080001;
-        public static final int left_drawer=0x7f080009;
-        public static final int rel_layout=0x7f08000c;
-        public static final int setting_button=0x7f08000a;
-        public static final int settings_text=0x7f08000b;
-        public static final int subject=0x7f08000f;
-        public static final int timeDisplay=0x7f080004;
-        public static final int title=0x7f08000d;
+        public static final int action_accept=0x7f0a0013;
+        public static final int action_new=0x7f0a0011;
+        public static final int action_settings=0x7f0a0012;
+        public static final int cards_feed=0x7f0a0008;
+        public static final int content_frame=0x7f0a0007;
+        public static final int dateDisplay=0x7f0a0002;
+        public static final int deleteReminder=0x7f0a0003;
+        public static final int delete_button=0x7f0a0010;
+        public static final int description=0x7f0a000e;
+        public static final int drawer_layout=0x7f0a0006;
+        public static final int editText=0x7f0a0000;
+        public static final int editText2=0x7f0a0005;
+        public static final int editText5=0x7f0a0001;
+        public static final int left_drawer=0x7f0a0009;
+        public static final int rel_layout=0x7f0a000c;
+        public static final int setting_button=0x7f0a000b;
+        public static final int settings_text=0x7f0a000a;
+        public static final int subject=0x7f0a000f;
+        public static final int timeDisplay=0x7f0a0004;
+        public static final int title=0x7f0a000d;
     }
     public static final class layout {
         public static final int activity_edit_assignment=0x7f030000;
@@ -56,22 +61,32 @@ public final class R {
         public static final int drawer_list_item=0x7f030005;
     }
     public static final class menu {
-        public static final int main=0x7f070000;
-        public static final int menu_new_assignment=0x7f070001;
+        public static final int main=0x7f090000;
+        public static final int menu_new_assignment=0x7f090001;
     }
     public static final class string {
-        public static final int action_accept=0x7f050000;
-        public static final int action_new=0x7f050001;
-        public static final int action_settings=0x7f050002;
-        public static final int addReminder=0x7f050003;
-        public static final int app_name=0x7f050004;
-        public static final int deadline=0x7f050005;
-        public static final int drawer_close=0x7f050006;
-        public static final int drawer_open=0x7f050007;
-        public static final int hello_world=0x7f050008;
-        public static final int newAssignment=0x7f050009;
-        public static final int no=0x7f05000a;
-        public static final int yes=0x7f05000b;
+        public static final int action_accept=0x7f070000;
+        public static final int action_new=0x7f070001;
+        public static final int action_settings=0x7f070002;
+        public static final int addReminder=0x7f070003;
+        public static final int app_name=0x7f070004;
+        public static final int deadline=0x7f070005;
+        public static final int drawer_close=0x7f070006;
+        public static final int drawer_open=0x7f070007;
+        public static final int hello_world=0x7f070008;
+        public static final int menu_settings=0x7f070009;
+        public static final int newAssignment=0x7f07000a;
+        public static final int no=0x7f07000b;
+        public static final int pref_send_report=0x7f07000c;
+        public static final int pref_send_report_summary=0x7f07000d;
+        public static final int pref_sync_frequency=0x7f07000e;
+        public static final int pref_sync_frequency_summary=0x7f07000f;
+        public static final int pref_update_setting=0x7f070010;
+        public static final int pref_user_name=0x7f070011;
+        public static final int pref_user_name_summary=0x7f070012;
+        public static final int pref_user_profile=0x7f070013;
+        public static final int title_activity_main=0x7f070014;
+        public static final int yes=0x7f070015;
     }
     public static final class style {
         /** 
@@ -82,9 +97,12 @@ public final class R {
  API 11 theme customizations can go here. 
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f080000;
         /**  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppTheme=0x7f080001;
+    }
+    public static final class xml {
+        public static final int settings=0x7f040000;
     }
 }
