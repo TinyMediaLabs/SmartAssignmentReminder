12c486c1bde47c75ce1434b66e7fa212461b775b
JustasBelevicius <jube1997@gmail.com>
2015-05-08 14:21:51 +0300

    Added a useless comment for testing purposes.

--- build/generated/source/r/debug/com/coffeetocode/assignmentreminder/R.java
+++ build/generated/source/r/debug/com/coffeetocode/assignmentreminder/R.java
@@ -26,24 +26,26 @@ public final class R {
         public static final int ic_launcher=0x7f020007;
     }
     public static final class id {
-        public static final int action_accept=0x7f080011;
-        public static final int action_new=0x7f08000f;
-        public static final int action_settings=0x7f080010;
+        public static final int action_accept=0x7f080013;
+        public static final int action_new=0x7f080011;
+        public static final int action_settings=0x7f080012;
         public static final int cards_feed=0x7f080002;
         public static final int content_frame=0x7f080001;
-        public static final int dateDisplay=0x7f080007;
-        public static final int deleteReminder=0x7f080008;
-        public static final int delete_button=0x7f080004;
-        public static final int description=0x7f08000d;
+        public static final int dateDisplay=0x7f080008;
+        public static final int deleteReminder=0x7f080009;
+        public static final int delete_button=0x7f080010;
+        public static final int description=0x7f08000e;
         public static final int drawer_layout=0x7f080000;
-        public static final int editText=0x7f080005;
-        public static final int editText2=0x7f08000a;
-        public static final int editText5=0x7f080006;
+        public static final int editText=0x7f080006;
+        public static final int editText2=0x7f08000b;
+        public static final int editText5=0x7f080007;
         public static final int left_drawer=0x7f080003;
-        public static final int rel_layout=0x7f08000b;
-        public static final int subject=0x7f08000e;
-        public static final int timeDisplay=0x7f080009;
-        public static final int title=0x7f08000c;
+        public static final int rel_layout=0x7f08000c;
+        public static final int setting_button=0x7f080004;
+        public static final int settings_text=0x7f080005;
+        public static final int subject=0x7f08000f;
+        public static final int timeDisplay=0x7f08000a;
+        public static final int title=0x7f08000d;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
